de6c8960271ac5c200dfef8fad93d112b818ffd3
sartakov <svm@job.com>
2018-01-19 15:13:38 +0500

    Initial commit.

---
+++ core/src/main/java/svm/backend/dao/entity/UUIDEntity.java
@@ -0,0 +1,43 @@
+package svm.backend.dao.entity;
+
+import java.io.Serializable;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+@Data
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class UUIDEntity implements Serializable {
+    
+    @Id
+    @GeneratedValue(generator = "uuid-generator")
+    @GenericGenerator(name = "uuid-generator", strategy = "svm.backend.dao.generator.UUIDGenerator")
+    @Type(type = "uuid-binary")
+    @Column(length = 16)
+    protected UUID id;
+    
+    public static <T extends UUIDEntity> T newInstance(UUID id, Class<T> type) {
+        
+        T newInstance;
+        try {
+            newInstance = type.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        
+        newInstance.setId(id);
+        
+        return newInstance;
+        
+    }
+    
+}
+
