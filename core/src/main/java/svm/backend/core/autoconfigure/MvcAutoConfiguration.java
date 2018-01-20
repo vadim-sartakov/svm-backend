30a707bb3b74e013a6313fd63b4ba48e2517f14a
Вадим <vadim@home.ru>
2018-01-20 17:24:52 +0500

    Added mvc and cors autoconfigurations.

---
+++ core/src/main/java/svm/backend/core/autoconfigure/MvcAutoConfiguration.java
@@ -0,0 +1,30 @@
+package svm.backend.core.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcAutoConfiguration extends WebMvcConfigurerAdapter {
+        
+    public static void addLocalhostCors(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedHeaders("*")
+                .exposedHeaders("Location")
+                .allowedMethods("*");
+    }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        addLocalhostCors(registry);
+    }
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/{basePath:^(?!api|static).*$}/**").setViewName("index");
+    }
+      
+}
---
+++ core/src/main/java/svm/backend/core/autoconfigure/RepositoryRestAutoConfiguration.java
@@ -0,0 +1,27 @@
+package svm.backend.core.autoconfigure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.validation.Validator;
+
+@Configuration
+public class RepositoryRestAutoConfiguration extends RepositoryRestConfigurerAdapter {
+    
+    @Autowired
+    private Validator validator;
+
+    @Override
+    public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+        validatingListener.addValidator("beforeCreate", validator);
+        validatingListener.addValidator("beforeSave", validator);
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        MvcAutoConfiguration.addLocalhostCors(config.getCorsRegistry());
+    }
+    
+}
