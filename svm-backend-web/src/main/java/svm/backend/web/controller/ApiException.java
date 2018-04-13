0f09eba402e95c2e5d2eda87f6a56b2e07984495
Sartakov <Sartakov@sartakov.server.utek>
2018-04-13 17:32:45 +0500

    Web project enhancing.

--- svm-backend-web/src/main/java/svm/backend/web/controller/ApiException.java
+++ svm-backend-web/src/main/java/svm/backend/web/controller/ApiException.java
@@ -1,34 +1,21 @@
 package svm.backend.web.controller;
 
-import java.util.HashMap;
 import java.util.Map;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.Singular;
 import org.springframework.http.HttpStatus;
 
+@Data
+@Builder
+@RequiredArgsConstructor
 public class ApiException {
     
     private final HttpStatus status;
     private final String message;
-    private final Map<String, String> errors = new HashMap<>();
- 
-    public ApiException(HttpStatus status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-
-    public void addError(String field, String error) {
-        errors.put(field, error);
-    }
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public Map<String, String> getErrors() {
-        return errors;
-    }
     
+    @Singular
+    private final Map<String, String> errors;
+ 
 }
--- svm-backend-web/src/main/java/svm/backend/web/controller/RestExceptionHandler.java
+++ svm-backend-web/src/main/java/svm/backend/web/controller/RestExceptionHandler.java
@@ -3,7 +3,6 @@ package svm.backend.web.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.ConstraintViolationException;
-import javax.ws.rs.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -13,14 +12,12 @@ import org.springframework.data.rest.core.RepositoryConstraintViolationException
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -31,7 +28,10 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
        
     protected ResponseEntity<Object> sendException(String message, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
         if (body == null)
-            body = new ApiException(status, message);
+            body = ApiException.builder()
+                    .status(status)
+                    .message(message)
+                    .build();
         return new ResponseEntity<>(body, headers, status);
     }
     
@@ -61,11 +61,14 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
             WebRequest request) {
         
         String message = messageSource.getMessage("svm.backend.web.WrongObject", null, LocaleContextHolder.getLocale());
-        ApiException exception = new ApiException(HttpStatus.BAD_REQUEST, message);
+        ApiException.ApiExceptionBuilder exceptionBuilder = ApiException.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message(message);
         
         fieldErrors.forEach(fieldError ->
-                exception.addError(fieldError.getField(), fieldError.getDefaultMessage()));
+                exceptionBuilder.error(fieldError.getField(), fieldError.getDefaultMessage()));
         
+        ApiException exception = exceptionBuilder.build();
         return handleExceptionInternal(ex, exception, new HttpHeaders(), exception.getStatus(), request);
         
     }
@@ -95,27 +98,10 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     }
     
     @Override
-    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        String message = messageSource.getMessage("svm.backend.web.NotFound", null, LocaleContextHolder.getLocale());
-        return sendException(message, null, headers, status, request);
-    }
-
-    @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return handleConstraintViolation(ex, ex.getBindingResult().getFieldErrors(), request);
     }
-    
-    @ExceptionHandler(AccessDeniedException.class)
-    protected ResponseEntity<Object> handleAccessDenied(Exception ex, WebRequest request) {
-        String message = messageSource.getMessage("AbstractAccessDecisionManager.accessDenied", null, LocaleContextHolder.getLocale());
-        return sendException(message, null, new HttpHeaders(), HttpStatus.FORBIDDEN, request);
-    }
-    
-    @ExceptionHandler(BadRequestException.class)
-    protected ResponseEntity<Object> handleBadRequest(Exception ex, WebRequest request) {
-        return handleExceptionInternal(ex, null, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
-    }
-    
+
     /**
      * In none exception has been intercepted - throw internal server error.
      * @param ex
